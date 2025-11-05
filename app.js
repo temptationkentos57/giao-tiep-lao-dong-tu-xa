380f31bcdfb4c2acf11c7bdf3ad292cb25344d82
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:37:24 +0100

    Updated error handling middleware to respond with a JSON error message.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/api', routes);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).json({ error: 'Something broke!' });
 });
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
