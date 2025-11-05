924e32f3ae597845638c573c1ad6dec3657f7992
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:37:56 +0100

    Updated greeting message in the root route for clarity.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // Định nghĩa các route khởi đầu
 app.get('/', (req, res) => {
-  res.send('Chào mừng bạn đến với Giao Tiếp Lao Động Từ Xa! Hãy bắt đầu!');
+  res.send('Chào mừng bạn đến với hệ thống Giao Tiếp Lao Động Từ Xa! Hãy bắt đầu khám phá!');
 });
 
 app.listen(PORT, () => {
