9a1998dbd6e080c8a462c4a95e866e166429729b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:38:36 +0100

    Update message to standardize language use

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Kết nối tới MongoDB
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Kết nối tới MongoDB thành công'))
+  .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-// Định nghĩa các route khởi đầu
+// Define initial routes
 app.get('/', (req, res) => {
-  res.send('Chào mừng bạn đến với hệ thống Giao Tiếp Lao Động Từ Xa! Hãy bắt đầu khám phá!');
+  res.send('Welcome to the Remote Work Communication System! Start exploring!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Máy chủ đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
