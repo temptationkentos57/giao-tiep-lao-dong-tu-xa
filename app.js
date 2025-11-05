c7a7ab6df82b7e312d5b4bf819cb47fcade5542e
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:36:32 +0100

    Added error handling middleware for better error management.

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`)))
   .catch(err => console.log(err));
